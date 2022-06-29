585c10a5e5c95975683c6927ace4f2e43a48fc82
Permanent Record <mklersy33@gmail.com>
2022-06-29 15:35:42 +0200

    Ajout credit side

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function findCollections() {
     
     $.ajax(settings).done(function (response) {
         const cred = response.slice(-1)[0];
-        console.log(cred)
         response = response.slice(0, -1);
         response.forEach((element, key) => {
             const listeSons = element.sons;
@@ -44,11 +43,153 @@ function findCollections() {
         }); 
         $('.subGlobal').append(`
             <div class="generalElement">
-                <label for="credits">Ligne de crédits</label>
+                <label for="credits">Boutton pour crédits</label>
                 <input placeholder="Crédits" value="${cred.credits}" name="credits"/>
             </div>
-        `)
+        `);
+
+        cred.details.forEach((elem, index) => {
+            console.log(elem, index)
+            $('.subGlobal').append('<div class="topGeneralElementCredit" id="'+index+'" style="width:100%"><div class="creditTopGlobal"></div></div>');
+            console.log($('.topGeneralElementCredit'))
+            $('.topGeneralElementCredit#' + index + ' .creditTopGlobal').append(`
+                <div class="subGeneralElement titleCred">
+                    <label for="titleCred">Titre</label>
+                    <input placeholder="Crédits" value="${elem.title}" name="titleCred"/>
+                </div>
+            `);
+            elem.content.forEach((subElem, subIndex) => {
+                $('.topGeneralElementCredit#' + index + ' .creditTopGlobal').append(`
+                    <div class="generalElementCredit">
+                        <div class="deleteElementCredit">
+                            Supprimer
+                        </div>
+                        <div class="subGeneralElement">
+                            <label for="subTitleCred">Titre paragraphe</label>
+                            <input placeholder="Direction du département des éditions" value="${subElem.title}" name="subTitleCred"/>
+                        </div>
+                        <div class="subGeneralElement">
+                            <label for="subDescCred">Description paragraphe</label>
+                            <input placeholder="Benjamin Arranger" value="${subElem.desc}" name="subDescCred"/>
+                        </div>
+                    </div>
+                `);
+                if(elem.content.length - 1 == subIndex) {
+                    $('.topGeneralElementCredit#' + index).append(`
+                        <div class="button" onclick="addParagraphe(${index})">
+                            Ajouter une ligne
+                        </div>
+                    `);
+                }
+            });
+            // if(elem.logo.length !== 0) {
+                $('.topGeneralElementCredit#' + index).append(`
+                    <h3>Logo</h3>
+                    <div class="logoTopGlobal" style="width:100%"></div>
+                `)
+            // }
+            elem.logo.forEach((subElem, subIndex) => {
+                $('.topGeneralElementCredit#' + index + " .logoTopGlobal").append(`
+                    <div class="generalElementCredit">
+                        <div class="deleteElementCredit">
+                            Supprimer
+                        </div>
+                        <div class="subGeneralElement">
+                            <label for="urlLogo">Nom du logo</label>
+                            <input placeholder="bnfLogo.png" value="${subElem.url}" name="urlLogo"/>
+                        </div>
+                        <div class="subGeneralElement">
+                            <label for="widthLogo">Largeur du logo</label>
+                            <input placeholder="130" value="${subElem.width}" name="widthLogo"/>
+                        </div>
+                        <div class="subGeneralElement">
+                            <label for="heightLogo">Hauteur du logo</label>
+                            <input placeholder="48" value="${subElem.height}" name="heightLogo"/>
+                        </div>
+                    </div>
+                `);
+                if(elem.logo.length - 1 == subIndex) {
+                    $('.topGeneralElementCredit#' + index).append(`
+                        <div class="button" onclick="addLogo(${index})">
+                            Ajouter un logo
+                        </div>
+                    `);
+                }
+            })
+            if(elem.logo.length == 0) {
+                $('.topGeneralElementCredit#' + index + ' .logoTopGlobal').append(`
+                    <div class="button" onclick="addLogo(${index})">
+                        Ajouter un logo
+                    </div>
+                `);
+            }
+        })
+        const deleteButton = document.querySelectorAll('.deleteElementCredit');
+        deleteButton.forEach((elem, i) => {
+            elem.addEventListener('click', function(e) {
+                console.log(e)
+                e.target.removeEventListener('click', function() {})
+                const element = e.target.parentElement;
+                element.parentElement.removeChild(element)
+            }, false)
+        })
     });
+}
+
+function addLogo(index) {
+    $('.topGeneralElementCredit#' + index + " .logoTopGlobal").append(`
+    <div class="generalElementCredit">
+        <div class="deleteElementCredit">
+            Supprimer
+        </div>
+        <div class="subGeneralElement">
+            <label for="urlLogo">Nom du logo</label>
+            <input placeholder="bnfLogo.png" name="urlLogo"/>
+        </div>
+        <div class="subGeneralElement">
+            <label for="widthLogo">Largeur du logo</label>
+            <input placeholder="130" name="widthLogo"/>
+        </div>
+        <div class="subGeneralElement">
+            <label for="heightLogo">Hauteur du logo</label>
+            <input placeholder="48" name="heightLogo"/>
+        </div>
+    </div>
+`);
+const deleteButton = document.querySelectorAll('.deleteElementCredit');
+deleteButton.forEach((elem, i) => {
+    elem.addEventListener('click', function(e) {
+        e.target.removeEventListener('click', () => {})
+        const element = e.target.parentElement;
+        element.parentElement.removeChild(element)
+    })
+}, false)
+}
+
+function addParagraphe(index) {
+    $('.topGeneralElementCredit#' + index + ' .creditTopGlobal').append(`
+        <div class="generalElementCredit">
+            <div class="deleteElementCredit">
+                Supprimer
+            </div>
+            <div class="subGeneralElement">
+                <label for="subTitleCred">Titre paragraphe</label>
+                <input placeholder="Direction du département des éditions" name="subTitleCred"/>
+            </div>
+            <div class="subGeneralElement">
+                <label for="subDescCred">Description paragraphe</label>
+                <input placeholder="Benjamin Arranger" name="subDescCred"/>
+            </div>
+        </div>
+    `);
+    const deleteButton = document.querySelectorAll('.deleteElementCredit');
+    deleteButton.forEach((elem, i) => {
+        elem.addEventListener('click', function(e) {
+            e.target.removeEventListener('click', () => {})
+            const element = e.target.parentElement;
+            element.parentElement.removeChild(element)
+        })
+    }, false)
 }
 
 findCollections()
@@ -108,7 +249,45 @@ function generate() {
             )
         }
     }
-    myJson.push({"credits": $('input[name="credits"]').val()});
+    myJson.push(
+        {
+            "credits": $('input[name="credits"]').val(),
+            "details": []
+        }
+    );
+    document.querySelectorAll('.topGeneralElementCredit').forEach((elem, index) => {
+        const titre  = $(elem).children('.creditTopGlobal').children('.titleCred').children('input[name=titleCred]').val();
+        myJson[myJson.length - 1].details.push({
+            "title": titre,
+            "content": [],
+            "logo": []
+        });
+        const detailCredit = $(elem).children('.creditTopGlobal').children('.generalElementCredit');
+        console.log(detailCredit)
+        for(let index2 = 0; index2 < detailCredit.length; index2++) {
+            const element = detailCredit[index2];
+            const titre = $(element).children('.subGeneralElement').children('input[name="subTitleCred"]').val();
+            const desc = $(element).children('.subGeneralElement').children('input[name="subDescCred"]').val();
+            myJson[myJson.length - 1].details[index].content.push({
+                "title": titre,
+                "desc": desc
+            });
+        }
+        const logoCredits = $(elem).children('.logoTopGlobal').children('.generalElementCredit');
+        for(let index2 = 0; index2 < logoCredits.length; index2++) {
+            const element = logoCredits[index2];
+            const url = $(element).children('.subGeneralElement').children('input[name="urlLogo"]').val();
+            const width = $(element).children('.subGeneralElement').children('input[name="widthLogo"]').val();
+            const height = $(element).children('.subGeneralElement').children('input[name="heightLogo"]').val();
+            myJson[myJson.length - 1].details[index].logo.push({
+                "url": url,
+                "width": width,
+                "height": height
+            });
+        }
+
+
+    })
     $('.result').text(JSON.stringify(myJson))
     const blob = new Blob([JSON.stringify(myJson)], { type: "text/plain;charset=utf-8" });
     var isIE = false || !!document.documentMode;
@@ -124,6 +303,7 @@ function generate() {
         a.click();
         document.body.removeChild(a);
     }
+
 }
 
 function addElement(key) {
